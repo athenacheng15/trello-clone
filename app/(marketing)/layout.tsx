b3078e31a827b10f75a44a3846de54449e533cf2
yuchuncheng <yuchuncheng@nine-yi.com>
2023-12-01 16:21:02 +0800

    feat : create navbar and footer

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,9 +1,12 @@
+import { Footer } from './_components/footer';
+import { Navbar } from './_components/navbar';
+
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="h-full bg-slate-100">
-            {/* Navbar */}
-            <main>{children}</main>
-            {/* Footer */}
+            <Navbar />
+            <main className="bg-slate-100 pb-20 pt-40">{children}</main>
+            <Footer />
         </div>
     );
 };
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,25 +1,53 @@
 import { Medal } from 'lucide-react';
+import Link from 'next/link';
+import localFont from 'next/font/local';
+import { Poppins } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+
+const headingFont = localFont({
+    src: '../../public/fonts/font.woff2',
+});
+
+const textFont = Poppins({
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
 
 const MarketingPage = () => {
     return (
         <div className="flex flex-col items-center justify-center">
-            <div className="flex flex-col items-center justify-center">
+            <div
+                className={cn(
+                    'flex flex-col items-center justify-center',
+                    headingFont.className,
+                )}
+            >
                 <div className="mb-4 flex items-center justify-center rounded-full border bg-amber-100 p-4 uppercase text-amber-700 shadow-sm">
                     <Medal className="mr-2 h-6 w-6" />
                     NO 1 task management
                 </div>
-                <h1 className="text-center text-3xl text-neutral-800 md:text-6xl">
+                <h1 className="mb-6 text-center text-3xl text-neutral-800 md:text-6xl">
                     Taskify helps team move
                 </h1>
                 <div className="w-fit rounded-md bg-gradient-to-r from-fuchsia-600 to-pink-600 p-2 px-4 pb-4 text-3xl text-white md:text-6xl">
                     work forword
                 </div>
             </div>
-            <div className="ms-au mt-4 max-w-xs text-center text-sm text-neutral-400 md:max-w-2xl md:text-lg">
+            <div
+                className={cn(
+                    'mx-auto mt-4 max-w-xs text-center text-sm text-neutral-400 md:max-w-2xl md:text-xl',
+                    textFont.className,
+                )}
+            >
                 Collabrate, manage projects, and reach new productivity peaks.
                 From high rises to the home office, the way your team works is
                 unique - accomplish it all with Taskify.
             </div>
+            <Button className="mt-6" size="lg" asChild>
+                <Link href="/sign-up">Get Taskify for free</Link>
+            </Button>
         </div>
     );
 };
