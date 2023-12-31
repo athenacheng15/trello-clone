4b658a54579a0d96a76446e63e35d4152727bd3b
Athena Cheng <athenachengyc15@gmail.com>
2023-12-31 17:04:35 +0800

    feat : create form popover component and logic

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,6 +1,7 @@
 import { User2, HelpCircle } from 'lucide-react';
 
 import { Hint } from '@/components/hint';
+import { FormPopover } from '@/components/form/form-popover';
 
 export const BoardList = () => {
     return (
@@ -10,19 +11,21 @@ export const BoardList = () => {
                 Your Boards
             </div>
             <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
-                <div
-                    role="button"
-                    className="relative flex aspect-video h-full w-full flex-col items-center justify-center gap-y-1 rounded-sm bg-muted transition hover:opacity-75"
-                >
-                    <p className="text-sm">Create New Board</p>
-                    <span className="text-xs">5 remaining</span>
-                    <Hint
-                        sideOffset={40}
-                        description={`Free Workspace can have up to 5 open boards> For unlimited boards upgrade this workspaces.`}
+                <FormPopover sideOffset={10} side="right">
+                    <div
+                        role="button"
+                        className="relative flex aspect-video h-full w-full flex-col items-center justify-center gap-y-1 rounded-sm bg-muted transition hover:opacity-75"
                     >
-                        <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
-                    </Hint>
-                </div>
+                        <p className="text-sm">Create New Board</p>
+                        <span className="text-xs">5 remaining</span>
+                        <Hint
+                            sideOffset={40}
+                            description={`Free Workspace can have up to 5 open boards> For unlimited boards upgrade this workspaces.`}
+                        >
+                            <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
+                        </Hint>
+                    </div>
+                </FormPopover>
             </div>
         </div>
     );
