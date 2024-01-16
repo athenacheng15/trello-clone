e8d68c0a039f43908f530c08782b66f4a94e843e
yuchuncheng <yuchuncheng@nine-yi.com>
2024-01-16 12:04:14 +0800

    feat : list-header editing function

--- app/(platform)/(dashboard)/board/[boardId]/_component/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_component/list-header.tsx
@@ -1,17 +1,71 @@
 'use client';
+import type { ElementRef } from 'react';
+
+import { useState, useRef } from 'react';
 
 import { List } from '@prisma/client';
+import { useEventListener } from 'usehooks-ts';
+import { FormInput } from '@/components/form/form-input';
 
 interface ListHeaderProps {
     data: List;
 }
 
 export const ListHeader = ({ data }: ListHeaderProps) => {
+    const [title, setTitle] = useState(data.title);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const formRef = useRef<ElementRef<'form'>>(null);
+    const inputRef = useRef<ElementRef<'input'>>(null);
+
+    const enableEditing = () => {
+        setIsEditing(true);
+        setTimeout(() => {
+            inputRef.current?.focus();
+            inputRef.current?.select();
+        });
+    };
+
+    const disableEditing = () => {
+        setIsEditing(false);
+    };
+
+    const onKeyDown = (e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            formRef.current?.requestSubmit();
+        }
+    };
+
+    useEventListener('keydown', onKeyDown);
+
     return (
         <div className=" flex items-start justify-between gap-x-2 px-2 pt-2 text-sm font-semibold">
-            <div className=" h-7 w-full border-transparent px-2.5 py-1 text-sm font-medium">
-                {data.title}
-            </div>
+            {isEditing ? (
+                <form className="flex-1 px-[2px]">
+                    <input hidden id="id" name="id" value={data.id} />
+                    <input
+                        hidden
+                        id="boardId"
+                        name="boardId"
+                        value={data.boardId}
+                    />
+                    <FormInput
+                        id="title"
+                        placeholder="Enter list title..."
+                        ref={inputRef}
+                        onBlur={() => {}}
+                        defaultValue={title}
+                        className="h-7 truncate border-transparent bg-transparent px-[7px] py-1 text-sm font-medium transition hover:border-input focus:border-input focus:bg-white"
+                    />
+                </form>
+            ) : (
+                <div
+                    onClick={enableEditing}
+                    className=" h-7 w-full border-transparent px-2.5 py-1 text-sm font-medium"
+                >
+                    {title}
+                </div>
+            )}
         </div>
     );
 };
