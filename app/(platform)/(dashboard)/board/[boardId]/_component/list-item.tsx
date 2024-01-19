f25a696bdb1c2e4fefcfc60966d8e43175ba03e5
yuchuncheng <yuchuncheng@nine-yi.com>
2024-01-19 17:52:17 +0800

    feat : map card-item to list

--- app/(platform)/(dashboard)/board/[boardId]/_component/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_component/list-item.tsx
@@ -2,10 +2,12 @@
 
 import type { ElementRef } from 'react';
 import type { ListWithCards } from '@/types';
+import { cn } from '@/lib/utils';
 
 import { ListHeader } from './list-header';
 import { useRef, useState } from 'react';
 import { CardForm } from './card-form';
+import { CardItem } from './card-item';
 
 interface ListItemProps {
     data: ListWithCards;
@@ -31,6 +33,16 @@ export const ListItem = ({ data, index }: ListItemProps) => {
         <li className="h-full w-[272px] shrink-0 select-none">
             <div className=" w-full rounded-md bg-[#f1f2f4] pb-2 shadow-md">
                 <ListHeader onAddCard={enableEditing} data={data} />
+                <ol
+                    className={cn(
+                        'mx-1 flex flex-col gap-y-2 px-1 py-0.5',
+                        data.cards.length > 0 ? 'mt-2' : 'mt-0',
+                    )}
+                >
+                    {data.cards.map((card, index) => (
+                        <CardItem key={card.id} index={index} data={card} />
+                    ))}
+                </ol>
                 <CardForm
                     ref={textareaRef}
                     listId={data.id}
