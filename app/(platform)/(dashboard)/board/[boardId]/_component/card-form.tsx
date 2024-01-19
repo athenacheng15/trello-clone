a1ef2b8f7a28cf36ea67260b7a200a9014b8bf3f
yuchuncheng <yuchuncheng@nine-yi.com>
2024-01-19 15:37:58 +0800

    feat : apply form-textarea to card-form

--- app/(platform)/(dashboard)/board/[boardId]/_component/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_component/card-form.tsx
@@ -1,8 +1,11 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import { Plus } from 'lucide-react';
 import { forwardRef } from 'react';
+import { Plus, X } from 'lucide-react';
+
+import { FormSubmit } from '@/components/form/form-submit';
+import { FormTextarea } from '@/components/form/form-textarea';
+import { Button } from '@/components/ui/button';
 
 interface CardFormProps {
     listId: string;
@@ -13,6 +16,29 @@ interface CardFormProps {
 
 export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
     ({ listId, isEditing, enableEditing, disableEditing }, ref) => {
+        if (isEditing) {
+            return (
+                <form className="m-1 space-y-4 px-1 py-0.5">
+                    <FormTextarea
+                        id="title"
+                        onKeyDown={() => {}}
+                        ref={ref}
+                        placeholder="Enetr a title for this card..."
+                    />
+                    <input hidden id="listId" name="listId" value={listId} />
+                    <div className="flex items-center gap-x-1">
+                        <FormSubmit>Add card</FormSubmit>
+                        <Button
+                            onClick={disableEditing}
+                            size="sm"
+                            variant="ghost"
+                        >
+                            <X className="h-5 w-5" />
+                        </Button>
+                    </div>
+                </form>
+            );
+        }
         return (
             <div className="px-2 pt-2">
                 <Button
