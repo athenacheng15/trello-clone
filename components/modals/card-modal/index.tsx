346b25a281c686257db05cf014a06254ab727a91
yuchuncheng <yuchuncheng@nine-yi.com>
2024-01-24 15:19:13 +0800

    feat : create cardId api and fetcher function

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -1,16 +1,25 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
+
 import { Dialog, DialogContent } from '@/components/ui/dialog';
 import { useCardModal } from '@/hooks/use-card-modal';
+import { CardWithList } from '@/types';
+import { fetcher } from '@/lib/fetcher';
 
 export const CardModal = () => {
     const id = useCardModal(state => state.id);
     const isOpen = useCardModal(state => state.isOpen);
     const onClose = useCardModal(state => state.onClose);
 
+    const { data: cardData } = useQuery<CardWithList>({
+        queryKey: ['card', id],
+        queryFn: () => fetcher(`/api/cards/${id}`),
+    });
+
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
-            <DialogContent>I am a modal</DialogContent>
+            <DialogContent>{cardData?.title}</DialogContent>
         </Dialog>
     );
 };
