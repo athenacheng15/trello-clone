d481a808eaf517cd610526017ce56780c3ca35f2
yuchuncheng <yuchuncheng@nine-yi.com>
2024-01-24 15:03:46 +0800

    feat : install tanstack/react-query and create query provider

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -2,13 +2,16 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { Toaster } from 'sonner';
 
 import { ModalProvider } from '@/components/providers/modal-provider';
+import { QueryProvider } from '@/components/providers/query-provider';
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <ClerkProvider>
-            <Toaster />
-            <ModalProvider />
-            {children}
+            <QueryProvider>
+                <Toaster />
+                <ModalProvider />
+                {children}
+            </QueryProvider>
         </ClerkProvider>
     );
 };
