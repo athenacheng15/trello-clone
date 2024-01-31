fa1282f40b892b7a7cc5a7091ea8a0ea2b988cda
yuchuncheng <yuchuncheng@nine-yi.com>
2024-01-31 14:58:39 +0800

    feat : create delete-card action

---
+++ actions/delete-card/index.ts
@@ -0,0 +1,44 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+import { auth } from '@clerk/nextjs';
+
+import { db } from '@/lib/db';
+import { createSafeAction } from '@/lib/create-safe-atcion';
+
+import { DeleteCard } from './schema';
+import { InputeType, ReturnType } from './types';
+
+const handler = async (data: InputeType): Promise<ReturnType> => {
+    const { userId, orgId } = auth();
+
+    if (!userId || !orgId) {
+        return {
+            error: 'Unauthorized',
+        };
+    }
+
+    const { id, boardId } = data;
+
+    let card;
+
+    try {
+        card = await db.card.delete({
+            where: {
+                id,
+                list: {
+                    board: {
+                        orgId,
+                    },
+                },
+            },
+        });
+    } catch (error) {
+        return { error: 'Failed to delete.' };
+    }
+
+    revalidatePath(`/board/${boardId}`);
+    return { data: card };
+};
+
+export const deleteCard = createSafeAction(DeleteCard, handler);
