bfab63c88b2bec9cba067b48b7230575f5a5c00e
yuchuncheng <yuchuncheng@nine-yi.com>
2024-01-31 14:29:06 +0800

    feat : apply actions to description

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -1,15 +1,20 @@
 'use client';
 
+import type { CardWithList } from '@/types';
+
+import { ElementRef, useRef, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { useQueryClient } from '@tanstack/react-query';
+import { useEventListener, useOnClickOutside } from 'usehooks-ts';
+import { AlignLeft } from 'lucide-react';
+
+import { useAction } from '@/hooks/use-action';
+import { updateCard } from '@/actions/update-card';
 import { FormSubmit } from '@/components/form/form-submit';
 import { FormTextarea } from '@/components/form/form-textarea';
 import { Button } from '@/components/ui/button';
 import { Skeleton } from '@/components/ui/skeleton';
-import type { CardWithList } from '@/types';
-import { useQueryClient } from '@tanstack/react-query';
-import { AlignLeft } from 'lucide-react';
-import { useParams } from 'next/navigation';
-import { ElementRef, useRef, useState } from 'react';
-import { useEventListener, useOnClickOutside } from 'usehooks-ts';
+import { toast } from 'sonner';
 
 interface DescriptionProps {
     data: CardWithList;
@@ -42,11 +47,24 @@ export const Description = ({ data }: DescriptionProps) => {
     useEventListener('keydown', onKeyDown);
     useOnClickOutside(formRef, disableEditing);
 
+    const { execute, fieldErrors } = useAction(updateCard, {
+        onSuccess: data => {
+            queryClient.invalidateQueries({
+                queryKey: ['card', data.id],
+            });
+            toast.success(`Card ${data.title} updated`);
+            disableEditing();
+        },
+        onError: error => {
+            toast.error(error);
+        },
+    });
+
     const onSubmit = (formData: FormData) => {
         const description = formData.get('description') as string;
         const boardId = params.boardId as string;
 
-        //TODO execute
+        execute({ id: data.id, description, boardId });
     };
 
     return (
@@ -57,12 +75,14 @@ export const Description = ({ data }: DescriptionProps) => {
                     Description
                 </p>
                 {isEditing ? (
-                    <form ref={formRef} className="space-y-2">
+                    <form action={onSubmit} ref={formRef} className="space-y-2">
                         <FormTextarea
+                            ref={textareaRef}
                             id="description"
                             className="mt-2 w-full"
                             placeholder="Add a more detail description"
                             defaultValue={data.description || undefined}
+                            errors={fieldErrors}
                         />
                         <div className="flex items-center gap-x-2">
                             <FormSubmit>Save</FormSubmit>
