63f5839c6c102d546a480508b0f2efcb0fd19bf9
yuchuncheng <yuchuncheng@nine-yi.com>
2024-01-31 15:09:12 +0800

    feat : apply copy and delete action to card modal

--- components/modals/card-modal/actions.tsx
+++ components/modals/card-modal/actions.tsx
@@ -3,19 +3,63 @@
 import type { CardWithList } from '@/types';
 
 import { Copy, Trash } from 'lucide-react';
+import { useParams } from 'next/navigation';
+import { toast } from 'sonner';
 
+import { copyCard } from '@/actions/copy-card';
+import { deleteCard } from '@/actions/delete-card';
+import { useAction } from '@/hooks/use-action';
 import { Button } from '@/components/ui/button';
 import { Skeleton } from '@/components/ui/skeleton';
+import { useCardModal } from '@/hooks/use-card-modal';
+import { error } from 'console';
 
 interface ActionsProps {
     data: CardWithList;
 }
 
 export const Actions = ({ data }: ActionsProps) => {
+    const params = useParams();
+    const cardModal = useCardModal();
+    const { execute: executeCopyCard, isLoading: isLoadingCopy } = useAction(
+        copyCard,
+        {
+            onSuccess: data => {
+                toast.success(`Card "${data.title}" copied`);
+                cardModal.onClose();
+            },
+            onError: error => {
+                toast.error(error);
+            },
+        },
+    );
+    const { execute: executeDeleteCard, isLoading: isLoadingDelete } =
+        useAction(deleteCard, {
+            onSuccess: data => {
+                toast.success(`Card "${data.title}" deleted`);
+                cardModal.onClose();
+            },
+            onError: error => {
+                toast.error(error);
+            },
+        });
+
+    const onCopy = () => {
+        const boardId = params.boardId as string;
+        executeCopyCard({ id: data.id, boardId });
+    };
+
+    const onDelete = () => {
+        const boardId = params.boardId as string;
+        executeDeleteCard({ id: data.id, boardId });
+    };
+
     return (
         <div className="mt-2 space-y-2">
             <p className="text-xs font-semibold">Actions</p>
             <Button
+                onClick={onCopy}
+                disabled={isLoadingCopy}
                 variant="gray"
                 className="w-full justify-start"
                 size="inline"
@@ -24,6 +68,8 @@ export const Actions = ({ data }: ActionsProps) => {
                 Copy
             </Button>
             <Button
+                onClick={onDelete}
+                disabled={isLoadingDelete}
                 variant="gray"
                 className="w-full justify-start"
                 size="inline"
