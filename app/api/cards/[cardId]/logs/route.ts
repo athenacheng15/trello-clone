769080f8dcfefba2c65aa971437d6dc834a667ee
yuchuncheng <yuchuncheng@nine-yi.com>
2024-01-31 16:49:32 +0800

    feat : create auditLogs api

---
+++ app/api/cards/[cardId]/logs/route.ts
@@ -0,0 +1,31 @@
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
+import { db } from '@/lib/db';
+import { ENTITY_TYPE } from '@prisma/client';
+
+export async function GET(
+    req: Request,
+    { params }: { params: { cardId: string } },
+) {
+    try {
+        const { userId, orgId } = auth();
+        if (!userId || !orgId) {
+            return new NextResponse('Unauthorized', { status: 401 });
+        }
+
+        const auditLogs = await db.auditLog.findMany({
+            where: {
+                orgId,
+                entityId: params.cardId,
+                entityType: ENTITY_TYPE.CARD,
+            },
+            orderBy: {
+                createdAt: 'desc',
+            },
+            take: 3,
+        });
+    } catch (error) {
+        return new NextResponse('Internal Error', { status: 500 });
+    }
+}
