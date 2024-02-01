de067216fdffb973e1b21079d7124e1a692d3fb5
yuchuncheng <yuchuncheng@nine-yi.com>
2024-02-01 14:15:18 +0800

    feat : finished activity page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/_components/activity-list.tsx
@@ -0,0 +1,38 @@
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+import { db } from '@/lib/db';
+import { ActivityItem } from '@/components/activity-item';
+import { Skeleton } from '@/components/ui/skeleton';
+
+export const ActivityList = async () => {
+    const { orgId } = auth();
+    if (!orgId) redirect('/select-org');
+
+    const auditLogs = await db.auditLog.findMany({
+        where: { orgId },
+        orderBy: { createdAt: 'desc' },
+    });
+    return (
+        <ol className="mt-4 space-y-4">
+            <p className=" hidden text-center text-xs text-muted-foreground last:block">
+                No activity found inside this organization
+            </p>
+            {auditLogs.map(log => (
+                <ActivityItem key={log.id} data={log} />
+            ))}
+        </ol>
+    );
+};
+
+ActivityList.Skeleton = function ActivityListSkeleton() {
+    return (
+        <ol className="mt-4 space-y-4">
+            <Skeleton className="h-14 w-[80%]" />
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-14 w-[70%]" />
+            <Skeleton className="h-14 w-[80%]" />
+            <Skeleton className="h-14 w-[75%]" />
+        </ol>
+    );
+};
